2116858830342dfa5b6ab2484eb6b17f25630e78
Pete Russell <pinkbatz@gmail.com>
2020-05-11 22:35:05 +1200

    Added: site title, nav links, and basic styling.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+
 import './NavBar.scss';
-import { Container, Row, Col } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-        <Container fluid className="navBar">
-            <Row>
-                <Col>Hello</Col>
-                <Col>Again</Col>
-            </Row>
+        <Container fluid className="nav-bar py-3">
+            <Container fluid="md">
+                <Row>
+                    <Col xs="auto">
+                        <span className="site-title">GroundSchool NZ</span>
+                    </Col>
+                    <Col>
+                        <Nav>
+                            <Nav.Item>
+                                <Nav.Link href="/exams">Exams</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link href="/contact">Contact</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Row>
+            </Container>
         </Container>
     );
 };
